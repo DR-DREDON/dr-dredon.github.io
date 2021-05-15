f1b446820942decdca0679840e9be9f85e70ec20
Dmitry Ivanov <dr.dredon@gmail.com>
2021-05-15 23:44:15 +0300

    4 резюме

--- script.js
+++ script.js
@@ -1,49 +1,16 @@
-function Max()
-{
-d=document
-var a = Number(d.form1.A.value) 
-var b = Number(d.form1.B.value) 
-var x1 = Math.max(a,b) 
-d.form1.x1.value=x1;
+var  name="Иванов Дмитрий Ильич";
+document.getElementById("SecondName").innerHTML= name;
+function rename(){
+    var input = document.getElementById('txt').value;
+    document.getElementById('SecondName').innerHTML=input;
 }
 
-function Check()
+function Rnd()
 {
-d=document
-var admin, name; 
-name = "Арина";
-admin = name;
-alert( admin ); 
-}
-
- functionAge()
-{
-    d=document
-    let name = prompt("Введите имя"),
-            age = +prompt('Введите ваш возраст');
- 
-        alert(`Здравствуйте, ${name}!`);
-        let message = (age < 18) ? 'Вы младше 18 лет' : 'Вы старше 18 лет';
-        alert(message);
-
-        var result = confirm('Вы уверены, что вам '+age);
-        if (result == true)
-        {
-            alert ('Хорошо')
-        }
-        else 
-        {
-            age = +prompt('Введите ваш возраст');
-            alert ('ОК')
-        }
-}
-
-function Show()
-{
-    d=document
-    var city, country;
-    city = "Москва";
-    country = "Россия";
-    alert( country );
-    alert( city );
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.getElementById('Pizza').style.backgroundColor=color
 }
